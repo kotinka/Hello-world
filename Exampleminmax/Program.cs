4d2862a79e0528af981b31eb7adc46fd3eae059e
kotinka <kotinka@list.ru>
2022-10-07 09:56:16 +0300

    minmax

---
+++ Exampleminmax/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = -9;
+int b = -3;
+
+int max = a;
+int min = b;
+
+if (b > max )  {max = b; min = a;}
+
+Console.Write("Большее число - ");
+Console.WriteLine( max);
+Console.Write("Меньшее число - ");
+Console.WriteLine( min);
+
+
