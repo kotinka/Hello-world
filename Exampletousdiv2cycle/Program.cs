8b1b1fcc03a9202ad43062c44c6e8d1311e225d5
kotinka <kotinka@list.ru>
2022-10-08 15:47:29 +0300

    complet

---
+++ Exampletousdiv2cycle/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write($"Введите одно число: ");
+int n = 1;
+string number = Console.ReadLine()!;
+n = int.Parse(number);
+
+int i = 1;
+while (i <= n)
+{int b = i%2;
+if (b == 0)
+
+    {Console.WriteLine($"{i}");
+    
+    }
+     i+=1;
+}
